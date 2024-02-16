c21f408ea58c64807a06d516cac38f720d2a1425
Max Dietrich <mail@mxd.codes>
2024-02-16 14:42:32 +0100

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,23 +1,31 @@
 module.exports = ({ env }) => ({
-    //...
-    comments: {
-      enabled: true,
-      config: {
-        badWords: false,
-        moderatorRoles: ["Authenticated"],
-        approvalFlow: ["api::page.page"],
-        entryLabel: {
-          "*": ["Title", "title", "Name", "name", "Subject", "subject"],
-          "api::page.page": ["MyField"],
-        },
-        blockedAuthorProps: ["name", "email"],
-        reportReasons: {
-          MY_CUSTOM_REASON: "MY_CUSTOM_REASON",
-        },
-        gql: {
-          // ...
-        },
+  upload: {
+    config: {
+      breakpoints: {
+        cover: 1300,
+        content: 670,
+        thumbnail: 350,
       },
     },
-    //...
-  });
+  },
+  comments: {
+    enabled: true,
+    config: {
+      badWords: false,
+      moderatorRoles: ["Authenticated"],
+      approvalFlow: ["api::page.page"],
+      entryLabel: {
+        "*": ["Title", "title", "Name", "name", "Subject", "subject"],
+        "api::page.page": ["MyField"],
+      },
+      blockedAuthorProps: ["name", "email"],
+      reportReasons: {
+        MY_CUSTOM_REASON: "MY_CUSTOM_REASON",
+      },
+      gql: {
+        // ...
+      },
+    },
+  },
+  //...
+});
