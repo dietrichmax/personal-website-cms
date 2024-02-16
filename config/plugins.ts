dcfc32327612c42889eeafb92001037883cef3a4
Max Dietrich <mail@mxd.codes>
2024-02-16 21:03:56 +0100

    blur image

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,6 +8,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
   "strapi-blurhash": {
     enabled: true,
     config: {
